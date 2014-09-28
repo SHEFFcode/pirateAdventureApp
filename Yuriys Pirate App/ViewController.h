097dc116326885544355b22f122bdc64caf1b4c7
Yuriy <sheff_ne@yahoo.com>
2014-09-28 10:55:17 -0500

    Initial Commit

---
+++ Yuriys Pirate App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Yuriys Pirate App
+//
+//  Created by Yuriy Shefer on 9/28/14.
+//  Copyright (c) 2014 SHEFFcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Yuriys Pirate App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Yuriys Pirate App
+//
+//  Created by Yuriy Shefer on 9/28/14.
+//  Copyright (c) 2014 SHEFFcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
